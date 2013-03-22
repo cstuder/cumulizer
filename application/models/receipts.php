f6c21b0c0dac3782f82e5060c31e5221c132157f
christian studer <cstuder@existenz.ch>
2013-03-22 18:21:11 +0100

    Adding mock data

--- application/models/receipts.php
+++ application/models/receipts.php
@@ -31,6 +31,14 @@ class Receipts extends CI_Model {
 		
 		// TODO finish this
 		
+		// Mock data
+		$purchases[] = array('datetime' => strtotime('2011-05-27 20:28:00'), 'itemname' => 'MB KARTOFFELGNOCCHI  700G', 'quantity' => 1, 'discount' => 0, 'price' => 2.75, 'categoryid' => 10);
+		$purchases[] = array('datetime' => strtotime('2011-05-27 20:28:00'), 'itemname' => 'AGNESI ELICHE', 'quantity' => 1, 'discount' => 0.45, 'price' => 1.6, 'categoryid' => 10);
+		$purchases[] = array('datetime' => strtotime('2011-05-27 20:28:00'), 'itemname' => 'BIO ITALIEN.SALAMI   MAXI', 'quantity' => 0.102, 'discount' => 1.05, 'price' => 4.15, 'categoryid' => 10);
+		$purchases[] = array('datetime' => strtotime('2011-05-27 20:28:00'), 'itemname' => ';M-CLAS MAYONNAISE 170G TB', 'quantity' => 1, 'discount' => 0, 'price' => 1.30, 'categoryid' => 2);
+		$purchases[] = array('datetime' => strtotime('2011-05-27 20:28:00'), 'itemname' => 'M-CLAS COTTAGE CHEESE NAT', 'quantity' => 1, 'discount' => 0, 'price' => 1.60, 'categoryid' => 9);
+		$purchases[] = array('datetime' => strtotime('2011-05-27 20:28:00'), 'itemname' => 'GURKEN TP         ST S.', 'quantity' => 1, 'discount' => 0, 'price' => 1.60, 'categoryid' => 9);
+		
 		return $purchases;
 	}
 	
@@ -46,6 +54,20 @@ class Receipts extends CI_Model {
 		
 		// TODO finish this
 		
+		// Mock data
+		$spendings[] = array(10 => 22.30, 3 => 11.40);
+		$spendings[] = array(10 => 2.30, 3 => 11.40);
+		$spendings[] = array(10 => 223.30, 4 => 11.40);
+		$spendings[] = array(10 => 2.30, 4 => 110.40);
+		$spendings[] = array(10 => 42.30, 4 => 11.40);
+		$spendings[] = array(10 => 22.30, 3 => 11.40);
+		$spendings[] = array(10 => 12.30, 3 => 11.40);
+		$spendings[] = array(10 => 52.30, 3 => 1.40);
+		$spendings[] = array(10 => 32.30, 3 => 3.40);
+		$spendings[] = array(10 => 22.30, 3 => 1.40);
+		$spendings[] = array(9 => 22.30, 3 => 11.50);
+		$spendings[] = array(10 => 22.30, 3 => 11.40);
+		
 		return $spendings;
 	}
 	
@@ -58,6 +80,15 @@ class Receipts extends CI_Model {
 		$summary = array();
 		
 		// TODO finish this
+		
+		// Mock data
+		$summary = array(
+			'numberOfPurchases' => 614,
+			'inSeasonFruits' => 33,
+			'meat' => 12,
+			'totalPrice' => 1234.40,
+			'totalDiscount' => 42.20,
+		);
 		
 		return $summary;
 	}
