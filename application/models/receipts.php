9ce9d9e4cb52011096c8b98ecb71d543a4f62ff5
christian studer <cstuder@existenz.ch>
2013-03-22 17:32:22 +0100

    Preparing the API methods model methods

--- application/models/receipts.php
+++ application/models/receipts.php
@@ -15,6 +15,50 @@ class Receipts extends CI_Model {
 	 * - Store all lines in the items database
 	 */
 	public function newUploadedReceipt($filename) {
+		// TODO finish this
+	}
 	
+	
+	/**
+	 * Get a list of all purchases for this month
+	 * 
+	 * @param int $year
+	 * @param int $month
+	 * @return array
+	 */
+	public function getMonthlyPurchases($year, $month) {
+		$purchases = array();
+		
+		// TODO finish this
+		
+		return $purchases;
+	}
+	
+	/**
+	 * Get a sum of all purchases per month and category
+	 * 
+	 * @param int $startyear
+	 * @param int $startmonth
+	 * @param int $numberOfMonths
+	 */
+	public function action_spendings($startyear, $startmonth, $numberOfMonths) {
+		$spendings = array();
+		
+		// TODO finish this
+		
+		return $spendings;
+	}
+	
+	/**
+	 * Get a summary of the purchases of this year
+	 * 
+	 * @param int $year
+	 */
+	public function getYearlySummary($year) {
+		$summary = array();
+		
+		// TODO finish this
+		
+		return $summary;
 	}
 }
