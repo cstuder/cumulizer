dc83541a8125c664eb80c97136de2c75e80d8e4a
Colin Frei <colin.frei@liip.ch>
2013-03-23 11:16:29 +0100

    Add Guzzle using Composer

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -1,5 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+use Guzzle\Http\Client;
+
 class Dashboard extends CI_Controller {
 
 	/**
--- index.php
+++ index.php
@@ -191,6 +191,8 @@ if (defined('ENVIRONMENT'))
 		define('APPPATH', BASEPATH.$application_folder.'/');
 	}
 
+require_once 'vendor/autoload.php';
+
 /*
  * --------------------------------------------------------------------
  * LOAD THE BOOTSTRAP FILE
